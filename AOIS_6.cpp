2d924cfe388333079a2e43d3f1ef32aebffe263e
StoleYourFridge <stoleyourfridge@gmail.com>
2021-11-23 00:48:41 +0300

    push function modified

--- AOIS_6.cpp
+++ AOIS_6.cpp
@@ -25,6 +25,7 @@ void Table_Note::print()
 class Hash_table
 {
     int table_size;
+    int current_notes_amount;
     vector<Table_Note*>table;
     bool encode_checker(string &key_word);
     int numeral_key_word(string &key_word);
@@ -39,7 +40,7 @@ public:
     void find_with_key(string& key_word);
     void output();
 };
-Hash_table::Hash_table(int table_size) : table(table_size, NULL), table_size(table_size) {}
+Hash_table::Hash_table(int table_size) : table(table_size, NULL), table_size(table_size), current_notes_amount(0) {}
 bool Hash_table::encode_checker(string& key_word)
 {
     int min_key_word_size = 2;
@@ -65,8 +66,25 @@ void Hash_table::push(string& key_word, string& data)
     Note->data = data;
     Note->numeral_key_word = numeral_key_word(key_word);
     Note->hash = hash_function(Note->numeral_key_word);
-    Note->next = table[Note->hash];
-    table[Note->hash] = Note;
+    Note->next = NULL;
+    Table_Note* example = table[Note->hash], *previous = NULL;
+    while (example != NULL)
+    {
+        if (example->key_word == key_word) {
+            cout << Note ->key_word << " deny!" << endl;
+            delete Note;
+            return;
+        }
+        previous = example;
+        example = example->next;
+    }
+    if (previous == NULL) {
+        table[Note->hash] = Note;
+    }
+    else {
+        previous->next = Note;
+    }
+    current_notes_amount++;
 }
 void Hash_table::delete_with_key(string& key_word)
 {
@@ -78,11 +96,13 @@ void Hash_table::delete_with_key(string& key_word)
             table[hash_we_try_to_delete] = deleting->next;
             delete deleting;
             deleting = table[hash_we_try_to_delete];
+            current_notes_amount--;
         }
         else if (deleting->key_word == key_word && previous != NULL) {
             previous->next = deleting->next;
             delete deleting;
             deleting = previous->next;
+            current_notes_amount--;
         }
         else {
             previous = deleting;
@@ -102,6 +122,7 @@ void Hash_table::find_with_key(string& key_word)
 }
 void Hash_table::output()
 {
+    cout << "Total notes : " << current_notes_amount << endl;
     for (int i = 0; i < table.size(); i++)
     {
         Table_Note* example = table[i];
@@ -134,16 +155,18 @@ Hash_table::~Hash_table()
 int main()
 {
     Hash_table math(35);
-    string a = "Rectangle";
+    string a = "Reangle";
     string a_opr = "ctvybumiop,";
-    string b = "btnbdf";
+    string b = "Rengle";
     string b_opr = "tv7ybuniomp,";
-    string c = "rtbuin";
+    string c = "Recaftangle";
+    string c_opr = "tv7ybuniomp,";
+    string d = "rtbuin";
     math.push(a, a_opr);
     math.push(b, b_opr);
+    math.push(c, c_opr);
     math.output();
-    cout << "------------------------------" << endl;
-    math.delete_with_key(b);
+    math.delete_with_key(c);
     math.output();
     return 0;
 }
